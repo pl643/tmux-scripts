e9f7902b5894bc06096d1d1935557fd1e9774fc6
Peter Ly <peter.wt.ly@gmail.com>
2022-08-02 14:48:48 -0700

    refactor pop-fzf-selector

--- tmux-popup-fzf-selector.sh
+++ tmux-popup-fzf-selector.sh
@@ -9,9 +9,7 @@
 realpath="$(realpath $0)"                                                           
 # restart script in a popup
 if [ "$1" != "--no-popup" ]; then 
-    echo "" >> /tmp/db
-    echo start >> /tmp/db $(date) >> /tmp/db
-    set -x
+    #set -x
     tmux popup -E -h 10 -T '─[ <Enter> paste ] [ C-d copy to buffer ] [ <C-e> edit ] [ <C-s> splits -_/= ]-' "$realpath --no-popup" 
     tmp_file="/tmp/.fzf.edit"
     # for editing option
@@ -33,13 +31,15 @@ if [ "$1" != "--no-popup" ]; then
     fi
     exit 0
 fi 
+# --fzf-query
 
-# --include-all-tmux-panes : default to only panes in current owindow
+# --include-all-tmux-panes-history : could take a while if you have a lot of panes with long history
+# --include-active-window-tmux-panes : default to only panes in current owindow
 # pane_list_command="tmux list-panes -a" ALL windows, could be slow
     # else
-pane_list_command="tmux list-panes"  # only current window
+#pane_list_command="tmux list-panes"  # only current window
 
-fzf_selector() {
+fzf_with_options() {
     size=$(stty size)
     pane_lines=${size% *}
     let fzf_height=$(($pane_lines - 1))
@@ -49,7 +49,7 @@ fzf_selector() {
     if [ "${lines[0]}" = ctrl-s ]; then
         #ctrl_s_sel=$(echo ${lines[1]} | fzf --height $fzf_height)
         ctrl_s_words="$(echo "${lines[1]}" | sed  -e 's![[:space:]]\+!\n!g' -e 's![-/_=]\+!\n!g' | sort -u)"
-        fzf_selector "$ctrl_s_words"
+        fzf_with_options "$ctrl_s_words"
         exit
     fi
 
@@ -75,11 +75,11 @@ fzf_selector() {
     [ -z "$sel" ] || echo -n "$sel" | tmux load-buffer - \; paste-buffer
 }
 
-#perl_sort_uniq() {
-#
-#}
+get_pane_contents() {
+    pane_content_options="--active-pane-content"
+    pane_content_options="--all-pane-content"
+    pane_content_options="--limit-pane-history"
 
-default_fzf_contents() {
     if [ -z $ALL_PANES_OPTION ]; then
         list_panes="tmux list-panes"
     else
@@ -88,43 +88,45 @@ default_fzf_contents() {
 
     # iterate through all selected panes
     for pane in $($list_panes | cut -f 7 -d " "); do
-        echo pane: $pane >> /tmp/db
-        current_pane_lines=$(tmux capture-pane -t $pane -pS -)
-        [ -z all_pane_lines ] && all_pane_lines="$current_pane_lines" || \
-                all_pane_lines="$all_pane_lines$current_pane_lines"
+        echo $pane >> /tmp/db
+        tmux capture-pane -t $pane -pS -
+        #current_pane_lines=$(tmux capture-pane -t $pane -pS -)
+        #[ -z $all_pane_lines ] && all_pane_lines="$current_pane_lines" || \
+        #        all_pane_lines="$all_pane_lines$current_pane_lines"
     done
-
-    # splits lines into words
-    echo split lines int words >> /tmp/db
-    date >> /tmp/db
-    #all_pane_words="$(echo $all_pane_lines | sed -e 's/[[:space:]]\+/\n/g' | sort -u )"
-    # all_pane_words="$(echo $all_pane_lines | sed -e 's/[[:space:]]\+/\n/g' )"
-    # all_pane_words="$(perl -e ' @words = split /\s+/, $ARGV[0]; foreach(@words) { $words{$_}++;}foreach $word (keys %words ) {print "$word\n";}' \"$all_pane_lines\" )"
-    date >> /tmp/db
-
-    files_contents=$(cat ~/.bash_history)
-    all_pane_lines="$all_pane_lines$files_contents"
-    all_pane_lines="$(perl -e ' @words = split /\s+/, $ARGV[0]; foreach(@words) { $words{$_}++;}foreach $word (keys %words ) {print "$word\n";}' \"$all_pane_lines\" )"
-
-    fzf_contents="$all_pane_lines"
 }
 
 # INIT - define custom fzf_contents
 init_file="$HOME/.init.fzf-popup-selector"
 [ -f "$init_file" ] && source "$init_file"
-[ -z "$fzf_contents" ] && default_fzf_contents
-##echo (/per
+#[ -z "$fzf_contents" ] && default_fzf_contents
 
-echo openvim files >> /tmp/db
-date >> /tmp/db
-all_open_vim_files=$(cat $(lsof | grep nvim | awk '{ print $9 }' | grep .swp|cut -f2- -d%| sed -e 's!%!/!g' -e 's!.swp!!' -e 's!^!/!') | sed -e 's/[[:space:]]\+/\n/g' | sort -u )
-date >> /tmp/db
-echo all_open_vim_files wc: $(echo $all_open_vim_files|wc) >> /tmp/db
-echo "$all_open_vim_files" > /tmp/all_open_vim_files
-fzf_contents="$fzf_contents$all_open_vim_files"
-echo wc fzfcontents: $(echo "$fzf_contents"|wc ) >> /tmp/db
-fzf_selector "$fzf_contents"
+#all_open_vim_files=$(cat $(lsof | grep nvim | awk '{ print $9 }' | grep .swp|cut -f2- -d%| sed -e 's!%!/!g' -e 's!.swp!!' -e 's!^!/!') | sed -e 's/[[:space:]]\+/\n/g' | sort -u )
+#echo "$all_open_vim_files" > /tmp/all_open_vim_files
+fzf_contents="$fzf_contents$all_open_vim_files" 
+pane_contents="$(get_pane_contents)"
+pane_words="$(perl -e '@words = split /\s+/, $ARGV[0]; foreach(@words) { $words{$_}++;}foreach $word (keys %words ) {print "$word\n";}' \"$pane_contents\" )"
+#all_pane_words="$(perl -e \
+#    '@words = split /\s+/, $ARGV[0]; \
+#    foreach(@words) { \
+#        $words{$_}++; \
+#    } \
+#    foreach $word (keys %words ) { \
+#        print "$word\n";  \
+#    }' \
+#    \"$all_pane_lines\" )"
+
+echo "$pane_contents" > /tmp/pane_contents
+fzf_with_options "$pane_contents$pane_words"
 # TEST555
 # 'open files in vi/nvim
 # lsof | grep nvim | awk '{ print $9 }' | grep .swp|cut -f2- -d%| sed -e 's!%!/!g' -e 's!.swp!!' -e 's!^!/!'
-# very-long-variable-is-here 
+# splits lines into words
+# all_pane_words="$(echo $all_pane_lines | sed -e 's/[[:space:]]\+/\n/g' | sort -u )"
+# all_pane_words="$(echo $all_pane_lines | sed -e 's/[[:space:]]\+/\n/g' )"
+# all_pane_words="$(perl -e ' @words = split /\s+/, $ARGV[0]; foreach(@words) { $words{$_}++;}foreach $word (keys %words ) {print "$word\n";}' \"$all_pane_lines\" )"
+
+#files_contents=$(cat ~/.bash_history)
+#all_pane_lines="$all_pane_lines$files_contents"
+#all_pane_lines="$(perl -e ' @words = split /\s+/, $ARGV[0]; foreach(@words) { $words{$_}++;}foreach $word (keys %words ) {print "$word\n";}' \"$all_pane_lines\" )"
+
