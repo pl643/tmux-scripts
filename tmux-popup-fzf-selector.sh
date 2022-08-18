d218951571141f0976885353fd2519e394f7a746
Peter Ly <peter.wt.ly@gmail.com>
2022-08-18 13:35:20 -0700

    added more options

--- tmux-popup-fzf-selector.sh
+++ tmux-popup-fzf-selector.sh
@@ -8,18 +8,19 @@ realpath="$(realpath $0)"
 # restart script in a popup
 if [ "$1" != "--no-popup" ]; then 
     #set -x
-    tmux popup -E -h 10 -T '─[ <Enter> paste+<Enter> ] [ C-d copy to buffer ] [ <C-e> edit ] [ <C-s> splits -_/= ]-' "$realpath --no-popup" 
+    tmux popup -E -h 10 -T '─[ <Enter> paste+<Enter> ] [ A-c copy to buffer ] [ <C-e> edit ] [ <C-s> splits -_/= ]-' "$realpath --no-popup" 
     tmp_file="/tmp/.fzf.edit"
     # for editing option
     # popup -T '─[ <Enter> accept edit and paste <q> - cancel ]-'
     # script to run after 
     if [ -f $tmp_file ]; then
         tmux popup -E -h 6 \
-            -T '─[ <Enter> accept edit and paste <q> - cancel ]-' \
+            -T '─[ <Enter> paste+Enter <;p> paste <;q> - cancel ]-' \
             nvim \
             -c 'inoremap <CR> <Esc>:x<cr>'  \
             -c 'nnoremap <CR> :x<cr>'  \
             -c 'nnoremap q    :%d<cr>:x!<cr>' \
+            -c 'nnoremap M-v  :%d<cr>:x!<cr>' \
             "$tmp_file"
                     if [ -s $tmp_file ]; then  # if size is not zero. 
                         echo -n $(cat $tmp_file) | tmux load-buffer -
@@ -35,7 +36,7 @@ fzf_with_options() {
     pane_lines=${size% *}
     let fzf_height=$(($pane_lines - 1))
     echo ""
-    readarray -t lines < <(echo "$1" | fzf --height=$fzf_height --expect ctrl-d --expect ctrl-e --expect ctrl-s)
+    readarray -t lines < <(echo "$1" | fzf --height=$fzf_height --expect alt-c --expect alt-f --expect alt-v --expect alt-e --expect ctrl-s)
 
     if [ "${lines[0]}" = ctrl-s ]; then
         #ctrl_s_sel=$(echo ${lines[1]} | fzf --height $fzf_height)
@@ -44,15 +45,28 @@ fzf_with_options() {
         exit
     fi
 
-    ### COPY C-c ###
+    ### COPY A-c ###
     # copy to tmux clipboard
     if [ "${lines[0]}" = alt-c ]; then
         echo -n $(echo "${lines[1]}") | tmux load-buffer -
         exit
     fi
 
-    ### EDIT C-e ###
-    if [ "${lines[0]}" = ctrl-e ]; then
+    ### EDIT A-e ###
+    if [ "${lines[0]}" = alt-e ]; then
+        tmp_file="/tmp/.fzf.edit"
+        echo ${lines[1]} > $tmp_file 
+        #edit_selection="${lines[1]}"
+        tmux set-env edit-selection "${lines[1]}"
+        #nvim $tmp_file
+        #echo -n $(cat $tmp_file) | tmux load-buffer -
+        #rm $tmp_file
+        #tmux paste-buffer
+        exit
+    fi
+
+    ### EDIT A-f ###
+    if [ "${lines[0]}" = alt-f ]; then
         tmp_file="/tmp/.fzf.edit"
         echo ${lines[1]} > $tmp_file 
         #nvim $tmp_file
@@ -62,9 +76,16 @@ fzf_with_options() {
         exit
     fi
 
+    ### COPY A-v ###
+    # paste selected
+    if [ "${lines[0]}" = alt-v ]; then
+        echo -n $(echo "${lines[1]}") | tmux load-buffer - \; paste-buffer 
+        exit
+    fi
+
     ### Enter ###
     sel="${lines[1]}"
-    [ -z "$sel" ] || echo -n "$sel" | tmux load-buffer - \; paste-buffer
+    [ -z "$sel" ] || echo -n "$sel" | tmux load-buffer - \; paste-buffer \; send-key Enter
 }
 
 # INIT - define custom fzf_contents
